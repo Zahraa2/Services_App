45b07d7ddcfe197f743ba31a705dd1e54bd6c43e
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2022-11-02 23:47:04 +0200

    Environment-Created

---
+++ Web-Api/Services.DAL/Data/Context/ServicesContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DAL
+{
+    public class ServicesContext : DbContext
+    {
+        public ServicesContext(DbContextOptions<ServicesContext> options) : base(options)
+        {
+
+        }
+    }
+}
